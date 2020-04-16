9d1626b5ae1ef6654e1a4c141bda2c1f757b1567
Haley Kelling <haley@kellings.com>
2020-04-16 21:49:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,3 +10,18 @@ def loop_message_n_times(message, n)
     puts message
   end
 end
+
+def output_array(message, n)
+  n.times do
+    puts message
+  end
+end
+
+def return_string_array(array)
+  counter = 0
+  while count < array.length do
+    new_array = [] << array[0].to_s
+    counter += 1
+  end
+end
+  
